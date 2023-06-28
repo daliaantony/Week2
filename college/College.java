63882bc52cca111d61035ef4ac17163cbc4739c2
daliaantony <137043714+daliaantony@users.noreply.github.com>
2023-06-28 18:48:44 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("College Name method is being called from College class");
+	}
+	public void collegeCode() {
+		System.out.println("College code method is being called from College class");
+	}
+	public void collegeRank() {
+		System.out.println("College Rank method is called from College class");
+	}
+}

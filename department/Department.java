a785da36f6a1735dc1ddbdd149083fe0ff96f467
daliaantony <137043714+daliaantony@users.noreply.github.com>
2023-06-28 18:49:30 +0530

    Add files via upload

---
+++ department/Department.java
@@ -0,0 +1,10 @@
+package org.department;
+
+import org.college.*;
+
+public class Department extends College{
+	
+	public void deptName() {
+		System.out.println("Department name method is being called from Department class");
+	}
+}
